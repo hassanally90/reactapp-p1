fc4385b663cdbea1c63f3f7bd5d172373faff9b1
hassanally90 <hassan.ally.co@gmail.com>
2020-06-30 10:48:27 +0300

    Updates

---
+++ precache-manifest.aaf8db7ce1948f4c1971d9227fad2a80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b5f958c5867d2bfe5c095c4d100964",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a1903bc46606f554e4f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f4da81f5e1201f67e9f8",
+    "url": "/static/js/2.41e30962.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.41e30962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a1903bc46606f554e4f",
+    "url": "/static/js/main.f03f2009.chunk.js"
+  },
+  {
+    "revision": "5246f124eb8bcb9c41e1",
+    "url": "/static/js/runtime-main.8d4753d0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
